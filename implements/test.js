3795b03ddcfb59a34f378a6f3b273e89578932c9
fangyafen <fangyafenqidai@163.com>
2015-12-03 14:59:10 +0800

    捕获事件+depart request

--- implements/test.js
+++ implements/test.js
@@ -1,17 +1,54 @@
-var str="{well}";
-console.log(str.substr(str.indexOf('{'),str.indexOf('}')))
-var test={'Action': (0, 'move'), 'Position': (714, 736), 'MessageName': 'mouse 0 move'};
-console.log(JSON.stringify(test));
-var stream = require('stream');
-var Readable = require('stream').Readable;
+// var str="{well}";
+// console.log(str.substr(str.indexOf('{'),str.indexOf('}')))
+// var test={'Action': (0, 'move'), 'Position': (714, 736), 'MessageName': 'mouse 0 move'};
+// console.log(JSON.stringify(test));
+// var stream = require('stream');
+// var Readable = require('stream').Readable;
 
-var rs = new Readable;
-rs.push('beep ');
-rs.push('boop\n');
-//rs.push(null);
+// var rs = new Readable;
+// rs.push('beep ');
+// rs.push('boop\n');
+// //rs.push(null);
 
-rs.pipe(process.stdout);
+// rs.pipe(process.stdout);
 
 
-var duplex = new stream.Duplex();
+// var duplex = new stream.Duplex();
 
+var exec = require('child_process').exec;
+exec('df -P | awk \'NR > 1\'', function(err, stdout, stderr) {
+  if (err) {
+    console.log('disk err---' + err);
+    return callback(err);
+  }
+  var aDrives = [];
+  var aLines = stdout.split('\n');
+  for (var i = 0; i < aLines.length; i++) {
+    var sLine = aLines[i];
+    if (sLine != '') {
+      var aTokens = sLine.replace(/\s+/g, " ").split(/\s/g);
+      aDrives[aDrives.length] = {
+        filesystem: aTokens[0],
+        totalSize: aTokens[1],
+        used: aTokens[2],
+        available: aTokens[3],
+        capacity: aTokens[4],
+        mounted: aTokens[5]
+      };
+    }
+  }
+  //console.log(aDrives)
+  //callback(null, aDrives);
+});
+
+exec(
+  'xrandr',
+  function(err, stdout, stderr) {
+    if (err) console.log('xrandr error' + err);
+    else console.log(stdout.replace(/\s+/g, " ").split(/\s/g));
+  }
+);
+
+
+sLine = "hi    hi hi";
+console.log(sLine.replace(/\s+/g, " "));
--- test/testResourceManager.js
+++ test/testResourceManager.js
@@ -7,7 +7,7 @@ type.push('hardResource');
 //type.push('vga');
 args['type'] = type;
 args['desc'] = {};
-args['desc']['IP'] = '127.0.0.1';
+args['desc']['IP'] = '192.168.160.18';
 var resMgr = null;
 resMgr = resourceMgr.getResMgr();
 resMgr.on('hardResource', function(rst_) {
@@ -16,47 +16,47 @@ resMgr.on('hardResource', function(rst_) {
 });
 resMgr.getResourceList(function(err, ret_) {
   console.log("-------------->>>>>>>> " + JSON.stringify(ret_));
-  console.log(util.inspect(ret_, {colors:true, depth:10}));
-  var agrsObj = {};
-  agrsObj['desc'] = {};
-  agrsObj['type'] = ['hardResource'];
-  var detail = [];
-  var typeItem = {};
-  var type = [];
-  type.push('input');
-  type.push('camera');
-  typeItem['type'] = type;
-  typeItem['option'] = 0;
-  detail.push(typeItem);
-  typeItem = {};
-  type = [];
-  type.push('output');
-  type.push('audio');
-  typeItem['type'] = type;
-  typeItem['option'] = 0;
-  detail.push(typeItem);
-  typeItem = {};
-  type = [];
-  type.push('output');
-  type.push('video');
-  typeItem['type'] = type;
-  typeItem['option'] = 0;
-  detail.push(typeItem);
-  agrsObj['detail'] = detail;
-  agrsObj['desc'] = {};
-  agrsObj['other'] = 0;
-  agrsObj['desc']['IP'] = '127.0.0.1';
-  resMgr.applyResource(function(err, ret_) {
-    console.log("APPLY====================> " + err + "   " + JSON.stringify(ret_));
-    console.log('APPLY TIME--->' + new Date().getTime());
-    resMgr.getResourceList(function(err, ret_) {
-      console.log("GET-------------->>>>>>>> " + JSON.stringify(ret_));
-      resMgr.releaseResource(function(err, ret_) {
-        console.log("RELEASE====================> " + err + "   " + JSON.stringify(ret_));
-        resMgr.getResourceList(function(err, ret_) {
-          console.log("GET-------------->>>>>>>> " + JSON.stringify(ret_));
-        }, args);
-      }, agrsObj);
-    }, args);
-  }, agrsObj);
+  // console.log(util.inspect(ret_, {colors:true, depth:10}));
+  // var agrsObj = {};
+  // agrsObj['desc'] = {};
+  // agrsObj['type'] = ['hardResource'];
+  // var detail = [];
+  // var typeItem = {};
+  // var type = [];
+  // type.push('input');
+  // type.push('camera');
+  // typeItem['type'] = type;
+  // typeItem['option'] = 0;
+  // detail.push(typeItem);
+  // typeItem = {};
+  // type = [];
+  // type.push('output');
+  // type.push('audio');
+  // typeItem['type'] = type;
+  // typeItem['option'] = 0;
+  // detail.push(typeItem);
+  // typeItem = {};
+  // type = [];
+  // type.push('output');
+  // type.push('video');
+  // typeItem['type'] = type;
+  // typeItem['option'] = 0;
+  // detail.push(typeItem);
+  // agrsObj['detail'] = detail;
+  // agrsObj['desc'] = {};
+  // agrsObj['other'] = 0;
+  // agrsObj['desc']['IP'] = '127.0.0.1';
+  // resMgr.applyResource(function(err, ret_) {
+  //   console.log("APPLY====================> " + err + "   " + JSON.stringify(ret_));
+  //   console.log('APPLY TIME--->' + new Date().getTime());
+  //   resMgr.getResourceList(function(err, ret_) {
+  //     console.log("GET-------------->>>>>>>> " + JSON.stringify(ret_));
+  //     resMgr.releaseResource(function(err, ret_) {
+  //       console.log("RELEASE====================> " + err + "   " + JSON.stringify(ret_));
+  //       resMgr.getResourceList(function(err, ret_) {
+  //         console.log("GET-------------->>>>>>>> " + JSON.stringify(ret_));
+  //       }, args);
+  //     }, agrsObj);
+  //   }, args);
+  // }, agrsObj);
 }, args);
