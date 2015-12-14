360a9e799dd1ae20bf0c19adadd2db9245b9729e
fangyafen <fangyafenqidai@163.com>
2015-12-14 18:13:05 +0800

    monitor.js dir

--- implements/monitor.js
+++ implements/monitor.js
@@ -13,7 +13,7 @@ var curDir= process.cwd();
 function monitorFunc(callback) {
   ///home/yff/dde/servihce/hardresmgr/implements/pyxhook
   //free = spawn('python',['./pyxhook/monitor.py']); 
-  free = spawn('python', [curDir+'/pyxhook/monitor.py']);
+  free = spawn('python', [curDir+'/service/hardresmgr/implements/pyxhook/monitor.py']);
   // 捕获标准输出并将其打印到控制台 
   free.stdout.on('data', function(data) {
     //var rst=(''+data+'').replace(/(^\s*)|(\s*$)/g, "");
@@ -36,7 +36,7 @@ function monitorFunc(callback) {
 }
 
 function monitorPipe(callback) {
-  free = spawn('python', [curDir+'/pyxhook/monitor.py'],{ stdio: ['pipe', 'pipe', 'pipe'] });
+  free = spawn('python', [curDir+'/service/hardresmgr/implements/pyxhook/monitor.py'],{ stdio: ['pipe', 'pipe', 'pipe'] });
   return callback(null,free.stdout);
   // free.stdout.on('data', function(data) {
   //   //var rst=(''+data+'').replace(/(^\s*)|(\s*$)/g, "");
